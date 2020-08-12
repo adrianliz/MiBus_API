dcd3888549606698c465172a94d29098c0b2c193
Lizaga Isaac Adrián <759090@unizar.es>
2020-08-12 09:31:23 +0200

    feature: cors module

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import { BusesRouter } from './routes/busesRouter';
 import { BusesProxy } from './proxy/busesProxy';
+const cors = require('cors');
 
 export class App {
   private app: Express = express();
 
   constructor(port: Number) {
+    this.app.use(cors());
     this.initRoutes();
 
     this.app.listen(port, () => {
